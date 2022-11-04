db4a5cf7479c67457e2e743ecc9b6397705c8918
alerafart <maalejandrarafart@gmail.com>
2022-11-04 17:16:38 +0100

    read mp4 file and log progress %

---
+++ copy.js
@@ -0,0 +1,28 @@
+let fs = require('fs')
+let file = 'demo.mp4'
+
+let read = fs.createReadStream(file)
+fs.stat(file, (err,stat) => {
+
+    let total = stat.size
+    let progress = 0
+    let read = fs.createReadStream(file)
+
+    read.on('data', (chunk) => {
+        progress += chunk.length
+        console.log("j'ai lu " + Math.round(100 * progress / total) + "%")
+    })
+    
+    read.on('end', () => {
+        console.log("j'ai fini de lire le fichier")
+    } )
+
+})
+
+// fs.readFile('demo.mp4', (err, data)=> {
+//     if(err) throw err
+//     fs.writeFile('copy.mp4', data, (err) => {
+//         if(err) throw err
+//         console.log('le fichier a bien ete copié')
+//     })
+// })
